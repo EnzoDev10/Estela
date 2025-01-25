a9dbb93a3954c6b5fbaa180d8672c209969b473e
Enzo Cañete <enzodev4@gmail.com>
2025-01-25 16:28:16 -0300

    Added Sidebar and monaco editor.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <div className="flex">
         <FileNavigation />
         <main className="bg-zinc-700 w-full text-white">
+          <p className="h-[5vh]">Hola</p>
           <SnippetEditor />
         </main>
       </div>
