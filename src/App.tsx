1bea5dd4946bef5fe2aadb158a8dd604953a5545
Enzo Cañete <enzodev4@gmail.com>
2025-01-25 09:53:36 -0300

    Revert "Added editor and dependencies for side navigation"
    This reverts commit f43f236acc298b46f003b336a5d957974643851d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import "./App.css";
+import './App.css';
 
-import { FileNavigation, SnippetEditor } from "./components/index";
+import { SnippetEditor, SnippetList, SnippetForm } from './components/index';
 
 function App() {
-  return (
-    <>
-      <div className="flex">
-        <FileNavigation />
-        <main className="bg-zinc-700 w-full text-white">
-          <SnippetEditor />
-        </main>
-      </div>
-    </>
-  );
+	return (
+		<div className="bg-neutral-950 h-screen text-white grid grid-cols-12">
+			<div className="col-span-3 bg-teal-900">
+				<SnippetForm />
+				<SnippetList />
+			</div>
+			<div className="col-span-9 bg-teal-950 flex justify-center items-center">
+				<SnippetEditor />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Editor/SnippetEditor.tsx
+++ src/components/Editor/SnippetEditor.tsx
@@ -1,14 +1,7 @@
-import { Editor /* , DiffEditor, loader */ } from "@monaco-editor/react";
-
 export const SnippetEditor = () => {
-  return (
-    <>
-      <Editor
-        height="95vh"
-        defaultLanguage="javascript"
-        defaultValue='const example = "this is an example"'
-        theme="vs-dark"
-      />
-    </>
-  );
+	return (
+		<div>
+			<p>SnippetEditor</p>
+		</div>
+	);
 };
