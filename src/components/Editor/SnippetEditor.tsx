423da6aa7c221550e4e486a4e21fd028085b0fa6
Enzo Cañete <enzodev4@gmail.com>
2025-02-07 16:30:11 -0300

    Fixed update function
    used to update content even if there were no changes made.

--- src/components/Editor/SnippetEditor.tsx
+++ src/components/Editor/SnippetEditor.tsx
@@ -2,7 +2,6 @@ import { Editor } from '@monaco-editor/react';
 import { Button } from '../ui/button';
 import { useRef, useState } from 'react';
 
-/* Imports editor types to manipulate the content inside the */
 import type { editor } from 'monaco-editor';
 
 import { useSnippetsContext } from '@/App';
@@ -14,22 +13,23 @@ interface Props {
 }
 
 export const SnippetEditor = ({ currentSnippet }: Props) => {
+	// Ref to get the content of the editor.
 	const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
 	const { updateShownSnippets } = useSnippetsContext();
+
 	const [wereChangesMade, setWereChangesMade] = useState(false);
 
-	/* 
-  Declaring a variable just to update shown content inside editor.
-  Tried to also use it for editor Props but it gives errors.
-  */
+	// Declaring a variable just to update the content inside the editor.
+	// Tried to also use it for Props but it gave errors.
+
 	let snippet: Snippet;
 
 	if (currentSnippet) snippet = currentSnippet;
 
+	// function to update content of a snippet with the value inside editor.
 	async function editSnippetContent(newContent: string) {
 		try {
 			const db = await Database.load('sqlite:main.db');
-			// It uses a placeholder text so it is easier to add the code later.
 			await db.execute('UPDATE Snippets SET content = $1 WHERE id = $2', [
 				newContent,
 				snippet.id,
@@ -44,15 +44,18 @@ export const SnippetEditor = ({ currentSnippet }: Props) => {
 	}
 
 	function onClickHandle() {
-		const contentOfEditor = editorRef.current?.getValue();
-		if (contentOfEditor) {
-			snippet.content = contentOfEditor;
-			editSnippetContent(contentOfEditor);
-			setWereChangesMade(false);
+		if (wereChangesMade) {
+			const contentOfEditor = editorRef.current?.getValue();
+
+			if (contentOfEditor) {
+				snippet.content = contentOfEditor;
+				editSnippetContent(contentOfEditor);
+				setWereChangesMade(false);
+			}
 		}
 	}
 
-	/* The button only changes color when the content on the editor is not equal to the original content. */
+	/* Changes appearance only if the content of the editor is not equal to the original content. */
 	function checkIfChanged() {
 		if (currentSnippet?.content === editorRef.current?.getValue()) {
 			setWereChangesMade(false);
@@ -74,12 +77,9 @@ export const SnippetEditor = ({ currentSnippet }: Props) => {
 			/>
 			<Button
 				onClick={onClickHandle}
-				className={`absolute bottom-4 right-10 text-black ${
-					wereChangesMade ? 'bg-emerald-500' : ''
-				} `}
+				className='absolute bottom-4 right-10 bg-emerald-500 text-black'
 				variant='secondary'
-				/* If there is no snippet being edited, disable the button */
-				disabled={currentSnippet?.id ? false : true}
+				disabled={!wereChangesMade}
 			>
 				save
 			</Button>
